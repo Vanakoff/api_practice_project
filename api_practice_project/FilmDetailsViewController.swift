7496845b5dbb89a180be989cfeabb91e61478604
Vanakoff <andrey.vanakoff@gmail.com>
2022-02-10 23:50:49 +0100

    Functions for NetworkManager created (need to be updated)

--- api_practice_project/FilmDetailsViewController.swift
+++ api_practice_project/FilmDetailsViewController.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class FilmDetailsViewController: UIViewController {
-
+    
+    var films: [TarantinoFilm] = []
+    
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet var filmImageView: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var actorsLabel: UILabel!
+    @IBOutlet var yearLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
     }
     
+    private func fetchImage() {
+        guard let url = URL(string: Link.pulpFictionImage.rawValue) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                self.filmImageView.image = image
+                self.activityIndicator.stopAnimating()
+            }
+        } .resume()
+    }
+    
+//    private func fetchImage() {
+//        NetworkManager.shaerd.fetchImage(from: Link.pulpFictionImage.rawValue) { result in
+//            switch result {
+//            case .success(let data):
+//                self.filmImageView.image = UIImage(data: data)
+//                self.activityIndicator.stopAnimating()
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            }
+//        }
+//    }
 
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     
+     private func fetchImage() {
+         NetworkManager.shared.fetchImage(from: Link.imageURL.rawValue) { result in
+             switch result {
+             case .success(let data):
+                 self.imageView.image = UIImage(data: data)
+                 self.activityIndicator.stopAnimating()
+             case .failure(let error):
+                 print(error.localizedDescription)
+             }
+         }
+     }
+     
+    func fetchCoursesV2() {
+        NetworkManager.shared.fetch(dataType: [CourseV2].self,
+                                    from: Link.exampleFive.rawValue,
+                                    convertFromSnakeCase: false) { result in
+            switch result {
+            case .success(let courses):
+                self.coursesV2 = courses
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-    */
+     */
 
 }
--- api_practice_project/NetworkManager.swift
+++ api_practice_project/NetworkManager.swift
@@ -9,15 +9,104 @@ import Foundation
 
 enum Link: String {
     case pulpFiction = "https://www.omdbapi.com/?t=Pulp+Fiction&apikey=dfe96dbe"
+    case pulpFictionImage = "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
     case jackieBrown = "https://www.omdbapi.com/?t=jackie+brown&apikey=dfe96dbe"
     case killBill = "https://www.omdbapi.com/?t=kill+bill&apikey=dfe96dbe"
     case inglouriousBasterds = "https://www.omdbapi.com/?t=inglourious+basterds&apikey=dfe96dbe"
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
 }
 
 class NetworkManager {
     static let shaerd = NetworkManager()
     
     private init() {}
+    /*
+    func fetchImage(from url: String?, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+        guard let url = URL(string: url ?? "") else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else {
+                completion(.failure(.noData))
+                return
+            }
+            DispatchQueue.main.async {
+                completion(.success(imageData))
+            }
+        }
+    }
     
+    func fetch<T: Decodable>(dataType: T.Type, from url: String, convertFromSnakeCase: Bool = true, completion: @escaping(Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: url) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                if convertFromSnakeCase {
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                }
+                let type = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(type))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+     */
+    
+    func fetchImage(from url: String?, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+        guard let url = URL(string: url ?? "") else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else {
+                completion(.failure(.noData))
+                return
+            }
+            DispatchQueue.main.async {
+                completion(.success(imageData))
+            }
+        }
+    }
+    
+    func fetch<T: Decodable>(dataType: T.Type, from url: String, completion: @escaping(Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: url) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let type = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(type))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        } .resume()
+    }
     
 }
